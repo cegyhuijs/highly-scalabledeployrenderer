9d1023c8ccd428bac41ba6aab2742b94e2e22ea9
cegyhuijs <ssjcheavickerss@hotmail.com>
2024-05-10 19:27:58 +0700

    init project

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-function numIslands(grid) {
-  if (!grid || grid.length === 0) return 0;
-  const m = grid.length;
-  const n = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(grid, i, j);
-      }
-    }
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-  return islands;
-}
-function dfs(grid, i, j) {
-  const m = grid.length;
-  const n = grid[0].length;
-  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
-  grid[i][j] = "0";
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
+  return false;
 }
